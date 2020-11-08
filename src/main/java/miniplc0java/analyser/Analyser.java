bc2a6a4e7cab39b89627663163f20e7f68337fd9
Flame-Maple <63182541+Flame-Maple@users.noreply.github.com>
2020-11-08 22:27:35 +0800

    修改++

--- src/main/java/miniplc0java/analyser/Analyser.java
+++ src/main/java/miniplc0java/analyser/Analyser.java
@@ -448,7 +448,7 @@ public final class Analyser {
         } else if (check(TokenType.LParen)) {
             // 是表达式
             // 调用相应的处理函数
-            next();
+            expect(TokenType.LParen);
             analyseExpression();
             expect(TokenType.RParen);
         } else {
